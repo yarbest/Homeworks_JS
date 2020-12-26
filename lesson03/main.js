e98a6e45e1dd598b84bd1886f530dc0e0dab6d65
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-26 14:12:32 +0200

    1

--- lesson03/main.js
+++ lesson03/main.js
@@ -1,13 +1,3 @@
 'use strict';
 
-let num = 266219;
-num = num.toString().split('');
-let sum = 1;
-
-for (let elem of num) {
-    sum *= elem;
-}
-console.log(sum);
-sum = sum ** 3;
-console.log(sum);
-console.log(+sum.toString().slice(0, 2));
+console.log(2 * '2');
