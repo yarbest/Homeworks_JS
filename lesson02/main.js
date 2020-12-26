1164570169dce7163697da4181a09a56ea071fb0
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-26 13:46:45 +0200

    1

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,49 +1,4 @@
-// const elevatorAscend = (func) => {
-//     // эта функция выполняет асинхронную операцию. Сюда передается callback функция,
-//     //которая будет выполнена после завершения асинхронного действия
-//     console.log("I started to ascend");
-//     setTimeout(func, 5000);
-// };
-
-// const arrive = () => {
-//     console.log("I've arrived");
-//     console.log("I've stopped singing");
-//     console.log("I've stopped dancing");
-// };
-
-// const jam = () => {
-//     console.log("Elevator has stuck");
-// };
-
-// const sing = () => {
-//     console.log("I started to sing");
-// };
-
-// const dance = () => {
-//     console.log("I started to dance");
-// };
-
-// elevatorAscend(jam);
-// sing();
-// dance();
-
 'use strict';
-
-// const makePizza = function (title, cb) {
-//     console.log(`Заказ на приготовление пиццы «${title}» получен. Начинаем готовить…`);
-//     setTimeout(cb, 3000);
-// };
-
-// const readBook = function () {
-//     console.log('Читаю книгу «Колдун и кристалл»…');
-// };
-
-// const eatPizza = function () {
-//     console.log('Ура! Пицца готова, пора подкрепиться.');
-// };
-
-// makePizza('Пеперонни', eatPizza);
-// readBook();
 
 let num = 266219;
 num = num.toString().split('');
