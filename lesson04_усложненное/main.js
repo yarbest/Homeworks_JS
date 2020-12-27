df1a96b21b376703b6490f43696191c7c11b6623
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-27 18:01:36 +0200

    1

--- lesson04_усложненное/main.js
+++ lesson04_усложненное/main.js
@@ -1,41 +1,44 @@
 'use strict';
-let mission = 1000000;
-let salary = prompt('Your salary', 20000);
 
-let expenses1 = 'Food';
-let amount1 = 8300;
-let expenses2 = 'Games';
-let amount2 = 5200;
+const checkIfStringWithSlice = (str) => {
+    if (typeof str !== 'string') {
+        return "It's not a string";
+    }
+    console.log(str.split(''));
 
-let allExpenses = +amount1 + +amount2;
-console.log('Month Expenses', allExpenses);
-let remainingMoney = salary - allExpenses;
-console.log('Remaining Money', remainingMoney);
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] === ' ') {
+            str = str.slice(1);
+        }
+        if (str[str.length - 1] === ' ') {
+            str = str.slice(0, -1);
+        }
+    }
+    console.log(str.split(''));
 
-console.log(`You will achieve your goal in ${Math.ceil(mission / remainingMoney)} months`);
+    return str;
+};
 
-let everyDayExpenses = allExpenses / 30;
-console.log('You spend ' + everyDayExpenses + ' every day');
+const checkIfStringWithSplit = (str) => {
+    if (typeof str !== 'string') {
+        return "It's not a string";
+    }
+    str = str.split('');
+    console.log(str);
 
-if (everyDayExpenses >= 1200) {
-    console.log('You have high level of life');
-} else if (everyDayExpenses >= 600 && everyDayExpenses < 1200) {
-    console.log('You have medium level of life');
-} else if (everyDayExpenses >= 0 && everyDayExpenses < 600) {
-    console.log('You have low level of life');
-} else {
-    console.log('Something went wrong');
-}
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] === ' ') {
+            str.shift();
+        }
+        if (str[str.length - 1] === ' ') {
+            str.pop();
+        }
+    }
+    return str;
+};
 
-// let income = 'Freelance';
-// let addExpenses = prompt('Your possible expenses', 'Fare, Internet');
-// let deposit = confirm('Do you have a deposit?');
+// console.log(checkIfStringWithSlice('       Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello     '));
+// console.log(checkIfStringWithSlice(1));
 
-// let period = 4;
-// console.log(typeof money, typeof income, typeof deposit);
-// console.log('addExpenses.length ' + addExpenses.length);
-// console.log(`Период равен ${period} месяцев. Цель заработать ${mission} гривен`);
-// console.log(`Expenses ${addExpenses.toLowerCase().split(',')}`);
-
-// let budgetDay = salary / 30;
-// console.log(budgetDay);
+console.log(checkIfStringWithSplit('   Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello   '));
+console.log(checkIfStringWithSplit(1));
