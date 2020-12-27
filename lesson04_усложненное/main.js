b5fc85eb3d40f135a414652a23a22db5e1de0088
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-27 18:14:31 +0200

    1

--- lesson04_усложненное/main.js
+++ lesson04_усложненное/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const checkIfStringWithSlice = (str) => {
-    if (typeof str !== 'string') {
-        return "It's not a string";
-    }
-    console.log(str.split(''));
+// const checkIfStringWithSlice = (str) => {
+//     if (typeof str !== 'string') {
+//         return "It's not a string";
+//     }
+//     console.log(str.split(''));
 
-    for (let i = 0; i < str.length; i++) {
-        if (str[0] === ' ') {
-            str = str.slice(1);
-        }
-        if (str[str.length - 1] === ' ') {
-            str = str.slice(0, -1);
-        }
-    }
-    console.log(str.split(''));
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[0] === ' ') {
+//             str = str.slice(1);
+//         }
+//         if (str[str.length - 1] === ' ') {
+//             str = str.slice(0, -1);
+//         }
+//     }
+//     console.log(str.split(''));
 
-    return str;
-};
+//     return str;
+// };
 
 const checkIfStringWithSplit = (str) => {
     if (typeof str !== 'string') {
@@ -34,11 +34,17 @@ const checkIfStringWithSplit = (str) => {
             str.pop();
         }
     }
-    return str;
+
+    if (str.length > 10) {
+        console.log(str.length, str.length - 30);
+        str.splice(10, str.length - 10, '...');
+    }
+
+    return str.join('');
 };
 
 // console.log(checkIfStringWithSlice('       Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello     '));
 // console.log(checkIfStringWithSlice(1));
 
-console.log(checkIfStringWithSplit('   Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello   '));
+console.log(checkIfStringWithSplit("   Hello World, I'm fine, how are you?    "));
 console.log(checkIfStringWithSplit(1));
