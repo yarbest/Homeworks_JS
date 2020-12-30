5669f943d5de50892e54d0a3bee9ec0288a14dbd
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-30 23:34:30 +0200

    1

--- lesson06/main.js
+++ lesson06/main.js
@@ -45,24 +45,40 @@
 
 const guessNumber = (createdNumber) => {
     console.log(`created number: ${createdNumber}`); //проверяю, остается ли то же самое значение, при рекурсии, да, останется, так как мы не вызываем функцию рандома
+    let attempts = 1;
     return function (userNumber) {
-        console.log(`user number: ${userNumber}`);
         if (userNumber === null) {
             //при нажатии на отмену, вернется +null = 0
 
             return; // вернет undefined. +undefined = NaN
-        } else if (userNumber.trim() === '' || isNaN(+userNumber)) {
+        }
+        console.log(`user number: ${userNumber}`);
+        attempts--;
+        if (attempts === 0) {
+            let resetGame = confirm('Game is over. Wanna play again?');
+            if (resetGame === true) {
+                const startGame = guessNumber(Math.ceil(Math.random() * 20));
+                startGame(prompt(`Enter number`));
+            } else {
+                return;
+            }
+        }
+        if (userNumber.trim() === '' || isNaN(+userNumber)) {
             //Проверка на ввод пустых строк и не чисел
             //'           '.trim() = ''
             //+'       7 ' = 7; НО +'7       7' = NaN;
             //+'2d' = NaN
-            alert(`'${userNumber}' is not a Number`);
+            if (userNumber === null) {
+                return;
+            }
+
+            alert(`'${userNumber}' is not a Number. Left attempts: ${attempts}`);
             startGame(prompt(`Enter number`));
         } else if (+userNumber > createdNumber) {
-            alert('Your number is bigger');
+            alert(`Your number is bigger. Left attempts: ${attempts}`);
             startGame(prompt(`Enter number`)); //рекурсия, так как эта анонимная функция записана в переменную startGame
         } else if (+userNumber < createdNumber) {
-            alert('Your number is smaller');
+            alert(`Your number is smaller. Left attempts: ${attempts}`);
             startGame(prompt(`Enter number`));
         } else if (+userNumber === createdNumber) {
             alert('You won');
