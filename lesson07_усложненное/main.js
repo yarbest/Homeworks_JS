83d1e95f951e747f43972374aeafa824fa36690e
yarbest <72338354+yarbest@users.noreply.github.com>
2020-12-31 19:30:59 +0200

    Update main.js

--- lesson07_усложненное/main.js
+++ lesson07_усложненное/main.js
@@ -9,5 +9,16 @@ week.forEach(function (day) {
     }
 });
 var date = new Date();
-document.write(`<p><b>It's ${date.getDay()} day of a week</b></p>`);
+// document.write(`<p><b>It's ${date.getDay()} day of a week</b></p>`);
+let currentDay;
+switch (date.getDay()) {
+    case 4:
+        currentDay = 'Thursday';
+        break;
+    default:
+        currentDay = 'none';
+}
+console.log(currentDay);
+document.write(`<p><b>It's ${currentDay} now</b></p>`);
+
 document.write(`<b>It's ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} now</b>`);
