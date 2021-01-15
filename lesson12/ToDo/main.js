e7238c1999d4d1791d1fa68710ed46690aaf6898
yarbest <72338354+yarbest@users.noreply.github.com>
2021-01-15 16:11:48 +0200

    1

--- lesson12/ToDo/main.js
+++ lesson12/ToDo/main.js
@@ -1,8 +1,8 @@
 'use strict';
 //Шаблон дела
-const task = document.createElement('div');
-task.classList.add('task');
-task.insertAdjacentHTML(
+const taskTemplate = document.createElement('div');
+taskTemplate.classList.add('task');
+taskTemplate.insertAdjacentHTML(
     'beforeend',
     `<span class="task-text"></span>
     <button class="btn-del-task"><b>Delete</b></button>
@@ -18,21 +18,71 @@ const btnMarkTask = document.querySelector('.btn-mark-task');
 
 let tasks = document.querySelectorAll('.task');
 
-let unfinishedTasks; //массив с пунктами не выполненных дел
-let listWithTaskText = []; //массив с текстовым содержанием не выполненных дел
-
-//=====================================================================================
-//Выводим пункты из localStorage если они есть
-listWithTaskText = JSON.parse(localStorage.getItem('unfinishedTasks'));
-if (listWithTaskText) {
-    //если тут что-то есть
-    listWithTaskText.forEach((item) => {
-        const newTaskFromStorage = task.cloneNode(true);
-        newTaskFromStorage.querySelector('.task-text').textContent = item;
-        unfinishedTasksBlock.append(newTaskFromStorage);
+//Получаем инфу о заданиях из хранилища при запуске страницы
+let taskData = []; //Массив, в котором хранятся задания в виде объектов
+taskData = JSON.parse(localStorage.getItem('taskData'));
+if (taskData) {
+    //если массив не пустой
+    taskData.forEach((item) => {
+        //в item лежит объект, в котором текст задания и его статус
+        const taskFromStorage = taskTemplate.cloneNode(true); //создаем html-объкт с заданием из хранилища
+        taskFromStorage.querySelector('.task-text').textContent = item.action; //меняем текст задания исходя из текста, который находится в свойстве объекта
+        if (item.marked === true) {
+            //если в хранилище у объкта значение свойства marked === true, то значит это дело было отмечено и его нужно положить
+            //в блок с выполненными заданиями
+            taskFromStorage.classList.add('marked');
+            finishedTasksBlock.append(taskFromStorage);
+        } else {
+            unfinishedTasksBlock.append(taskFromStorage); //иначе в невыполненные
+        }
     });
 }
-//=====================================================================================
+
+//кладем в localStorage инфу о нашем деле
+const setTaskToStorage = (task) => {
+    //изначально хранилище пустое, и строка taskData = JSON.parse(localStorage.getItem('taskData'));
+    //которая находится выше, запишет в данную переменную - null, а нам нужно чтобы это был []
+    //поэтому, если эта переменная равна null, то присваиваем [], иначе оставляем как было
+    taskData = taskData === null ? [] : taskData;
+    taskData.push({
+        action: task.querySelector('.task-text').textContent,
+        marked: task.classList.contains('marked') ? true : false, //если пункт отмечен, то в свойство объекта marked записывается true
+    });
+    localStorage.setItem('taskData', JSON.stringify(taskData)); //перезаписываем хранилище
+};
+
+//убираем из localStorage инфу о нашем деле
+const removeTaskFromStorage = (task) => {
+    let indexOfRemovedObject = taskData.findIndex((item, i, arr) => {
+        //в item лежит объект
+        if (item.action === task.querySelector('.task-text').textContent) {
+            //находим в массиве объект, у которого текстовое значение = равно текстовому значению дела, которое хотим удалить
+            return true; //в итоге возвращается индекс нужного объекта в массиве
+        }
+    });
+    taskData.splice(indexOfRemovedObject, 1); //убираем из массива заданий, объект(дело), который удалил пользователь
+    //перезаписываем хранилище
+    localStorage.setItem('taskData', JSON.stringify(taskData));
+};
+
+//менянем состояние выполноненности задания в хранилище
+const changeTaskStatus = (task) => {
+    let indexOfChangedObject = taskData.findIndex((item) => {
+        //в item лежит обхект с инфой о задании
+        if (item.action === task.querySelector('.task-text').textContent) {
+            //находим в массиве объект, у которого текстовое значение = равно текстовому значению дела, которое хотим изменить
+            return true;
+        }
+    });
+    //меняем маркировку, которая раньше была у дела, внутри хранилища
+    if (task.classList.contains('marked')) {
+        taskData[indexOfChangedObject].marked = true; //меняем статус текущего объекта(дела)
+    } else {
+        taskData[indexOfChangedObject].marked = false;
+    }
+    //перезаписываем хранилище
+    localStorage.setItem('taskData', JSON.stringify(taskData));
+};
 
 //проверка блоков на пустоту
 const checkIfBlocksEmpty = () => {
@@ -56,45 +106,36 @@ const checkIfBlocksEmpty = () => {
 };
 checkIfBlocksEmpty(); //изначально у нас блоки без пунктов, поэтому нужно им добавить текст, что дел еще нет
 
-btnAddTask.addEventListener('click', () => {
+const addNewTask = () => {
     if (inputEnterTask.value === '') {
         return;
     }
-    //изначально у нас блоки пустые, и в них добавляется текст заглушка, но если мы добавляем пункт, то заглушку нужно убрать
-    const newTask = task.cloneNode(true);
-    const taskText = newTask.querySelector('.task-text');
-    taskText.textContent = inputEnterTask.value;
+    const newTask = taskTemplate.cloneNode(true);
+    newTask.querySelector('.task-text').textContent = inputEnterTask.value;
     inputEnterTask.value = '';
     unfinishedTasksBlock.append(newTask);
+    setTaskToStorage(newTask);
+    //изначально у нас блоки пустые, и в них добавляется текст заглушка, но если мы добавляем пункт, то заглушку нужно убрать
     checkIfBlocksEmpty();
+};
 
-    //============================================
-    //кладем в localStorage текст из наших дел
-    unfinishedTasks = document.querySelectorAll('.task:not([class ~= marked])');
-    listWithTaskText = [];
-    //собираем текстовое содержание не отмеченных дел в новый массив, и кладем его в хранилище
-    unfinishedTasks.forEach((item) => {
-        listWithTaskText.push(item.querySelector('.task-text').textContent);
-    });
-    localStorage.setItem('unfinishedTasks', JSON.stringify(listWithTaskText));
+btnAddTask.addEventListener('click', () => {
+    addNewTask();
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addNewTask();
+    }
 });
 
 taskBlocks.addEventListener('click', (event) => {
     let myTarget = event.target;
-    let myTargetClosest = myTarget.closest('.task');
+    let myTargetClosest = myTarget.closest('.task'); //все задание целиком, вместе текстом и кнопками
 
     if (myTarget.classList.contains('btn-del-task')) {
+        removeTaskFromStorage(myTargetClosest);
         myTargetClosest.remove();
-        //============================================
-        //удаляем из localStorage текстовое содержание дела, которые удалили
-        listWithTaskText.forEach((item, i, arr) => {
-            if (item === myTargetClosest.querySelector('.task-text').textContent) {
-                arr.splice(arr.indexOf(item), 1);
-            }
-        });
-        console.log(listWithTaskText);
-        localStorage.setItem('unfinishedTasks', JSON.stringify(listWithTaskText));
-        //============================================
         checkIfBlocksEmpty();
     }
 
@@ -102,15 +143,15 @@ taskBlocks.addEventListener('click', (event) => {
         //если у пункта еще нету класса "Отмечено", то этот пункт нужно Отметить и положить в выполненные дела
         myTargetClosest.classList.add('marked');
         finishedTasksBlock.append(myTargetClosest);
+        changeTaskStatus(myTargetClosest);
+
         checkIfBlocksEmpty();
     } else if (myTarget.classList.contains('btn-mark-task') && myTargetClosest.classList.contains('marked') === true) {
         //если у пункта есть класс "Отмечено", то этот пункт нужно вернуть в невыполненные дела, и забрать у него Отметину
         myTargetClosest.classList.remove('marked');
         unfinishedTasksBlock.append(myTargetClosest);
+        changeTaskStatus(myTargetClosest);
+
         checkIfBlocksEmpty();
     }
-    // localStorage.setItem('unfinished-tasks', unfinishedTasks);
 });
-
-// localStorage.setItem('unfinishedTasks', JSON.stringify(unfinishedTasks));
-// unfinishedTasks = JSON.parse(localStorage.getItem('unfinishedTasks'));
